d46d2c9e66b3de3eb9cac2f143057db739f05294
Hans Bockgaard <hasseoa@gmail.com>
2017-11-22 16:43:56 +0100

    add login related pages

--- src/main/java/com/example/controller/GameController.java
+++ src/main/java/com/example/controller/GameController.java
@@ -3,15 +3,18 @@ package com.example.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpSession;
 import java.sql.*;
 import java.util.Calendar;
 
+@Controller
 @SpringBootApplication
 public class GameController {
 
@@ -21,8 +24,25 @@ public class GameController {
 	}
 
 	@GetMapping("/ssp/")
-	public ModelAndView listBlogs() {
-		return new ModelAndView("ssp");
-
+	public ModelAndView showGame() {
+		System.out.println("HÄNDER DET NÅGOT");
+		return new ModelAndView("/ssp");
 	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+	@GetMapping("/newuser")
+	public String newUser() {
+		return "newuser";
+	}
+
+	@PostMapping("/newuser")
+	public String submit(HttpSession session, @RequestParam String username, @RequestParam String password) {
+		System.out.println(username);
+			return "index";
+	}
+
 }
