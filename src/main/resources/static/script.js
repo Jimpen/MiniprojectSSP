868be572faa475f40cf56ca1e785730440c9807f
Your Name <you@example.com>
2017-11-23 11:18:20 +0100

    progress in javascriptcode

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,20 +1,55 @@
+
+
+function userChoice(){
+    document.addEventListener("click", function () {
+		document.getElementById("stone");
+    } );
+    document.addEventListener("click", function () {
+		document.getElementById("scissor");
+    });
+    document.addEventListener("click", function () {
+    	document.getElementById("bag");
+
+    } );
+}
+
 function computerChoice	() {
 	var randomNumber = Math.random();
 
 	if (randomNumber <= 0.34) {
-		//return 'Sten'; //Show img = "sten" hide class = "images"
-        feedbackElem: document.getElementById("stone")
+		document.getElementById("stone")//return <img src="stone.jpg">; //Show img = "sten" hide class = "images"
+
 	} else if (randomNumber <= 0.67) {
-		//return 'Påse'; //Show img = "bag" hide class = "images"
-        feedbackElem: document.getElementById("bag")
+        document.getElementById("bag")//return <img src="bag.jpg">; //Show img = "bag" hide class = "images"
+
 	} else {
-		//return 'Sax'; //Show img = "scissor" hide class = "images"
-        feedbackElem: document.getElementById("scissor")
-        resetElem.classList.add("hidden");
+        document.getElementById("scissor")//return <img src="scissor.jpg">; //Show img = "scissor" hide class = "images"
+
 	}
 }
 
-function playerChoice() {
-	addEventListener()
+function compareChoice(spelare, dator) {
+
+	if (spelare === dator) {
+		return 'LIKA';
+	}
+	else if (spelare === sten) {
+        if (dator === sax) {
+            return message(spelare, dator, true);
+        } else {
+            return message(spelare, dator, false);
+        }
+    }	else if (spelare === sax) {
+            if (dator === sten) {
+                return message (spelare, dator, false);
+            } else {
+                return message (spelare, dator, true);
+            }
+        } else if (spelare === påse) {
+            if (dator === sten) {
+                return message (spelare, dator, true);
+            } else {
+                return message (spelare, dator, false);
+            }
+	}
 }
-  
